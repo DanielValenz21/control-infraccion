5e0f7b103113371eaa7d813b16f4ae54fc6edbac
Daniel Valenzuela <hetzondaniel46@gmail.com>
2025-04-11 22:03:36 -0600

    segundo commit

--- App.tsx
+++ App.tsx
@@ -5,13 +5,19 @@ import { store } from "./src/redux/store"
 import MainNavigator from "./src/navigation/MainNavigator"
 import { StatusBar } from "expo-status-bar"
 
+// 1. Importamos TailwindProvider
+import { TailwindProvider } from "nativewind"
+
 export default function App() {
   return (
-    <Provider store={store}>
-      <NavigationContainer>
-        <StatusBar style="auto" />
-        <MainNavigator />
-      </NavigationContainer>
-    </Provider>
+    // 2. Agregamos <TailwindProvider> como wrapper
+    <TailwindProvider>
+      <Provider store={store}>
+        <NavigationContainer>
+          <StatusBar style="auto" />
+          <MainNavigator />
+        </NavigationContainer>
+      </Provider>
+    </TailwindProvider>
   )
 }
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,17 +1,15 @@
+// src/screens/HomeScreen.js
 import { View, Text, TouchableOpacity } from "react-native"
 import { Camera } from "lucide-react-native"
 import { HomeScreenTheme as t } from "../theme/HomeScreenTheme"
-import { useTailwind } from "nativewind"  // si usas nativewind
 
 export default function HomeScreen() {
-  const { tw } = useTailwind()
-
   return (
-    <View style={tw(t.container)}>
-      <Text style={tw(t.title)}>Pantalla de Escaneo</Text>
-      <TouchableOpacity style={tw(t.button)}>
+    <View className={t.container}>
+      <Text className={t.title}>Pantalla de Escaneo</Text>
+      <TouchableOpacity className={t.button}>
         <Camera stroke="#fff" size={24} />
-        <Text style={tw(t.buttonText)}>Escanear Documento</Text>
+        <Text className={t.buttonText}>Escanear Documento</Text>
       </TouchableOpacity>
     </View>
   )
